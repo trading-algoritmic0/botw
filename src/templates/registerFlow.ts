d1340890390683fdb41cc0aa569b5bb07e7631fb
trading-algoritmic0 <car.ramoz@icloud.com>
2025-04-15 18:56:22 -0500

    Update registerFlow.ts

--- src/templates/registerFlow.ts
+++ src/templates/registerFlow.ts
@@ -11,19 +11,19 @@ const registerFlow = addKeyword(EVENTS.ACTION)
     async (ctx, ctxFn) => {
       if (ctx.body === "No, gracias!") {
         return ctxFn.endFlow(
-          "El registro fue cancelado, podes volver a escribirle al bot para registrarte"
+          "El registro fue cancelado, podés volver a escribirle al bot para registrarte."
         );
       } else if (ctx.body === "Si, quiero!") {
         await ctxFn.flowDynamic(
-          "Perfecto, voy a proceder a hacerte algunas preguntas"
+          "Perfecto, voy a proceder a hacerte algunas preguntas 📝"
         );
       } else {
-        return ctxFn.fallBack("Tenes que elegir alguna de las opciones!");
+        return ctxFn.fallBack("Tenés que elegir alguna de las opciones!");
       }
     }
   )
   .addAnswer(
-    `Primero, cual es tu nombre?`,
+    `Primero, ¿cuál es tu nombre?`,
     { capture: true },
     async (ctx, ctxFn) => {
       await ctxFn.flowDynamic("Perfecto " + ctx.body + "! 🙌");
@@ -31,20 +31,24 @@ const registerFlow = addKeyword(EVENTS.ACTION)
     }
   )
   .addAnswer(
-    `Ahora, cual es tu mail?`,
+    `Ahora, ¿cuál es tu mail?`,
     { capture: true },
     async (ctx, ctxFn) => {
       const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
       if (!emailRegex.test(ctx.body)) {
         return ctxFn.fallBack(
-          "Por favor, ingresa un correo electrónico válido. 📧"
+          "Por favor, ingresá un correo electrónico válido. 📧"
         );
       }
+
       const state = ctxFn.state.getMyState();
       await sheetsService.createUser(ctx.from, state.name, ctx.body);
-      await ctxFn.flowDynamic(
-        "Excelente! Tus datos ya fueron cargados, ya podes comenzar a utilizar el bot"
-      );
+
+      await ctxFn.flowDynamic([
+        "✅ ¡Excelente! Tus datos ya fueron cargados.",
+        "🤖 Soy un bot automatizado. Escribime lo que necesites o elegí una opción:",
+        "🧭 Escribí *Menú* para ver las opciones disponibles."
+      ]);
     }
   );
 
