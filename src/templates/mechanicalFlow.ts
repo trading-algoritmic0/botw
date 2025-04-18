0d19fb0916c88a68aed67008a483c32416c38f3f
trading-algoritmic0 <car.ramoz@icloud.com>
2025-04-17 23:17:33 -0500

    Update mechanicalFlow.ts

--- src/templates/mechanicalFlow.ts
+++ src/templates/mechanicalFlow.ts
@@ -76,6 +76,11 @@ const mechanicalFlow = addKeyword(['mecanica_general'])
                   id: "preguntas2",
                   title: "Tengo preguntas",
                   description: "Contactar con asesor"
+                },
+                {
+                  id: "preguntas3",
+                  title: "Tengo preguntas",
+                  description: "Contactar con asesor"
                 }
               ]
             }
